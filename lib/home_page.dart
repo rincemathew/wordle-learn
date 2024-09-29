4a29acc62453bf9ae4a3eddbb2c87a4686166c0a
Rince Mathew <rincemathewmanattu@gmail.com>
2024-09-29 21:13:03 +0530

    set up layout

---
+++ lib/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Wordle"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: Container(
+              color: Colors.yellow,
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
